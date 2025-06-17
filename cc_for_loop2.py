ceb00d9b14894063e567f9a7c3309711cbc0e4f2
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-06-17 18:32:35 -0400

    Code Challenge: For Loop 2

--- cc_for_loop2.py
+++ cc_for_loop2.py
@@ -1,5 +1,9 @@
-for x :
-        print("*")
-        print("**")
-        print("***")
-        print("****")
+for x in range(0, 7, 1):
+        if x in [0,6]:
+                print("*")
+        elif x in[1,5]: 
+                print("**")
+        elif x in [2,3]:
+                print("***")
+        else:
+                print("****")
