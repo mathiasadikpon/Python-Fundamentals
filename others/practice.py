6b9ca319d9bbc62fe076353fea8924e7a54d3d96
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-06-19 23:53:29 -0400

    add function to practice.py

--- others/practice.py
+++ others/practice.py
@@ -1,4 +1,19 @@
 def add(x,y):
     return x + y
+def sub(x,y):
+    return x - y
+def mul(x,y):
+    return x * y
+def div(x,y):
+    return x / y
+def mod(x,y):
+    return x % y
+def pow(x,y):
+    return x ** y
+def floordiv(x,y):
+    return x // y
 
+# Test the add function
 print(add(2,5))
+# Test the sub function
+print(sub(10,5))
