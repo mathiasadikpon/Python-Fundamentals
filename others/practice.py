c3e0ad3b6c5e863d12f756b255908a468242c754
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-06-19 23:55:33 -0400

    update practice.py

--- others/practice.py
+++ others/practice.py
@@ -17,3 +17,18 @@ def floordiv(x,y):
 print(add(2,5))
 # Test the sub function
 print(sub(10,5))
+# Test the mul function
+print(mul(3,4))
+# Test the div function
+print(div(10,2))
+# Test the mod function
+print(mod(10,3))
+# Test the pow function
+print(pow(2,3))
+# Test the floordiv function
+print(floordiv(10,3))
+# Test the functions with different values
+print(add(10,20))
+print(sub(100,50))
+print(mul(5,6))     
+print(div(20,4))
