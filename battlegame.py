ac05ef90a5cf9eb91cb971d255121c40a99548ba
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-06-21 15:00:47 -0400

    Task 1 complet

---
+++ battlegame.py
@@ -0,0 +1,34 @@
+#3) 4) Declare three variables with the following names: wizard, elf, human
+
+wizard = "Wizard"
+elf = "Elf"
+human = "Human"
+
+"""
+5) Declare three variables, set to Integer values that indicate the hp of each character.
+The hp of a wizard is 70
+The hp of an elf is 100
+The hp of a human is 150
+Example: wizard_hp = 70
+"""
+wizard_hp = 70
+elf_hp = 100
+human_hp = 150
+
+"""
+6) Declare three more variables having Integer values that indicate the damage of each character.
+The damage of a Wizard is 150
+The damage of an Elf is 100
+The damage of a Human is 20
+Example: wizard_damage = 150
+"""
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
+
+"""
+7) Also declare two variables that set the hp and damage for the Dragon - 300 hp and 50 damage.
+Example: dragon_hp = 300
+"""
+dragon_hp = 300
+dragon_damage = 50
