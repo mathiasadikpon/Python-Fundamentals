2135717fce4f5a424e4ba582c63d2eb6d1b64ba4
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-06-22 22:04:12 -0400

    Code Challenge: Star Staircase For Loop

--- cc_staircase_loop.py
+++ cc_staircase_loop.py
@@ -1,5 +1,5 @@
 stars = ""
-for i in range(1, 5, 2):
-    for j in range(0, 2, 1):
-        stars += "i"
-        print(j)
+for i in range(0, 5, 1):
+    for j in range(0, i, 1):
+        stars += "*"
+    print(stars)
