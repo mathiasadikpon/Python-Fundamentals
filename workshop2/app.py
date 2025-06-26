7e5bd4fd267b9c868128a54e25fecf813cf2c80d
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-06-26 00:09:02 -0400

    Task 2 completed

--- workshop2/app.py
+++ workshop2/app.py
@@ -8,3 +8,10 @@ def atm_menu(name):
     print("------------------------------------------")
     print("| 3.    Withdraw    | 4.    Logout       |")
     print("------------------------------------------")
+
+
+print("    === Automated Teller Machine ===    ")
+name = input("Enter name to register: ")
+pin = input("Enter PIN: ")
+balance = 0
+print(f"{name} has been registered with a starting balance of ${balance}")
