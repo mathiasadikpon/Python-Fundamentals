054b0d320bb2ec28bdedddb0efcffe6faea28499
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-02 23:35:08 -0400

    update homepage

---
+++ week 3/workshop3/donations_pkg/homepage.py
@@ -0,0 +1,18 @@
+"""
+# workshop3.homepage
+# Mathias Adikpon
+Juily, 5 2025
+"""
+
+def show_homepage():
+    print("")
+    print("          === DonateMe Homepage ===          ")
+
+    print("------------------------------------------")
+    print("| 1.    Login     | 2.    Register        |")
+    print("------------------------------------------")
+    print("------------------------------------------")
+    print("| 3.    Donate    | 4.    Show Donations  |")
+    print("------------------------------------------")
+    print("|            5.    Exit                   |")
+    print("------------------------------------------")
