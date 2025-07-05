6c0b26329c25405eafaeb8b27979d09ac159abbe
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-05 08:46:15 -0400

    update comments

--- week 3/workshop3/donations_pkg/user.py
+++ week 3/workshop3/donations_pkg/user.py
@@ -25,7 +25,7 @@ def register(database, username, password):
     """Register a new user if the username is not already in the database."""
     # case-insensitive username : convert to lowercase and strip whitespace
     username = username.strip().lower()
-    if not username:
+    if not username: # Check if username is empty string
         print("\nUsername must have at least one character.")
         return ""
     elif not (username[0].isalpha() and username.isalnum()):
@@ -34,7 +34,7 @@ def register(database, username, password):
     elif len(username) > 10:
         print("\nUsername must not exceed 10 characters.")
         return ""
-    elif not password:
+    elif not password: # Check if password is empty string
         print("\nPassword must have at least one character.")
         return ""
     elif len(password) < 5:
@@ -43,10 +43,10 @@ def register(database, username, password):
     elif username in database:
         print(f"\n{username.capitalize()} already registered.")
         return ""
-    elif username[0].isalpha() and username.isalnum():
+    elif username[0].isalpha() and username.isalnum(): #This can also use else statement
         print(f"\n{username.capitalize()} has been registered successfully!")
         return username
     else:
-        # Other invalid cases
+        # Other invalid cases/ however, this should not be reached due to previous checks
         print("\nInvalid registeration. Please try again.")
         return ""
