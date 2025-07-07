da70ca1702bf6d2dcac0154744287bc57a912f73
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-07 04:05:53 -0400

    queue

---
+++ week 4/queue2.py
@@ -0,0 +1,18 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+
+    def size(self):
+        return len(self.items)
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.size() == 0:
+            return None
+        return self.items.pop(0)
+
+    def show_queue(self):
+        print(self.items)
