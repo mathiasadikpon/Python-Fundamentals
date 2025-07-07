54b10dca83a834b55583f98d234dfe5e5f72947a
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-07 04:06:55 -0400

    Queues

--- week 4/queue2.py
+++ week 4/queue2.py
@@ -16,3 +16,18 @@ class Queue:
 
     def show_queue(self):
         print(self.items)
+
+
+q = Queue()
+q.enqueue('a')
+q.enqueue('b')
+q.enqueue('c')
+
+print("Pass" if (q.size() == 3) else "Fail")
+q.enqueue('d')
+print("Pass" if (q.size() == 4) else "Fail")
+
+print("Pass" if (q.dequeue() == 'a') else "Fail")
+print("Pass" if (q.dequeue() == 'd') else "Fail")
+print("Pass" if (q.size() == 2) else "Fail")
+q.show_queue()
