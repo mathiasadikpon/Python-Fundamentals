4c5a5529ce6dfe9a4f173ad903dcdd04752afa2e
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-08 09:35:23 -0400

    update

---
+++ week 4/cc_order_queue.py
@@ -0,0 +1,39 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+
+    def size(self):
+        return len(self.items)
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.size() == 0:
+            return None
+        return self.items.pop(0)
+
+    def show_queue(self):
+        print(self.items)
+
+
+class IceCreamShop:
+    def __init__(self, flavors):
+        self.flavors = flavors
+        self.orders = Queue()
+
+    
+    def take_order(self,customer:str, flavor:str, scoops:int):
+        if flavor not in self.flavors:
+            print(f"Sorry {customer}, we don't have {flavor} flavor.")
+            
+        if scoops < 1 or scoops > 3:
+            print("Choose between 1-3 sccops.")
+        else:
+            print("Order created!")
+            order = {"customer":customer, "flavor":flavor, "scoops":scoops}
+            self.orders.enqueue(order)
+    
+
+    def show_all_orders():
