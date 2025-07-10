f12ad988c41a9eac2f9a3a6a62a6abc23f43c746
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-10 00:00:05 -0400

    t

--- week 4/workshop4.py
+++ week 4/workshop4.py
@@ -5,8 +5,21 @@ class User:
         self.pin = pin
         self.password = password
 
+    def change_name(self, new_name):
+        self.name = new_name
+    
+    def change_pin(self, new_pin):
+        self.pin = new_pin
+    
+    def change_password(self, new_password):
+        self.password = new_password
 
+    
 """ Driver Code for Task 1 """
 # user "Bob" as the name, 1234 as the pin, and "password" as the password
 user = User("Bob", 1234, "password")
 print(f"{user.name} {user.pin} {user.password}")
+
+
+
+""" Driver Code for Task 2 """
