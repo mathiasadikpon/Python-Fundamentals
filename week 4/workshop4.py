6a21e92aa8437427501095f6ce8c5795f2dc5693
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-09 23:57:16 -0400

    update

--- week 4/workshop4.py
+++ week 4/workshop4.py
@@ -4,3 +4,9 @@ class User:
         self.name = name
         self.pin = pin
         self.password = password
+
+
+""" Driver Code for Task 1 """
+# user "Bob" as the name, 1234 as the pin, and "password" as the password
+user = User("Bob", 1234, "password")
+print(f"{user.name} {user.pin} {user.password}")
