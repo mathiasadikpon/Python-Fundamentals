970d9511bb9f4fa6acf5d54353f05928af58daff
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-11 19:27:23 -0400

    workshop 4

--- week 4/workshop4.py
+++ week 4/workshop4.py
@@ -23,7 +23,7 @@ class BankUser(User):
     
     def show_balance(self):
         """Prints the BankUser object's balance"""
-        print(f"{self.name.capitalize()} has an account balance of: {0 if self.balance else f'{self.balance:.1f}'}")
+        print(f"{self.name.capitalize()} has an account balance of: {f'{self.balance:.1f}' if self.balance else 0}")
 
     def withdraw(self, amount):
         """Withdraws the specified amount from the BankUser's balance"""
@@ -43,6 +43,49 @@ class BankUser(User):
             self.balance += amount
             # print(f"{self.name.capitalize()} deposited {amount:.1f}. New balance: {self.balance:.1f}")
 
+    def transfer_money(self, amount: 'float', receiver: 'BankUser'):
+        """Transfers the specified amount to another BankUser's balance"""
+        print()
+        print(f"You are transferring ${amount} to {receiver.name.capitalize()}")
+        # Authenticate the pin of the sender: sender is "self"
+        print("Authentication required")
+        pin = input("Enter your PIN: ").strip()
+        if pin.isdigit() and int(pin) == self.pin:
+            print("Transfer authorized")
+            print(f"Transferring ${amount} to {receiver.name.capitalize()}")
+            self.withdraw(amount)
+            receiver.deposit(amount)
+            return True
+        else:
+            print("Invalid PIN. Transaction canceled.")
+            return False
+            
+        
+
+    
+    def request_money(self, amount: 'float', sender: 'BankUser'):
+        """Requests the specified amount from another BankUser's balance bse on User receiving's pin and vlalide password of user requesting the money"""
+        print()
+        print(f"You are requesting ${amount} from {sender.name.capitalize()}")
+        # Authenticate the pin of the sender
+        print("User authentication is required...")
+        pin = input(f"Enter {sender.name.capitalize()}'s PIN: ").strip()
+        if pin.isdigit() and int(pin) == sender.pin:
+            # Authenticate the password of the receiver: receiver is "self"
+            password = input(f"Enter your password: ").strip()
+            if password == self.password:
+                print("Request authorized")
+                sender.withdraw(amount)
+                self.deposit(amount)
+                print(f"{sender.name.capitalize()} sent ${amount}")
+                return True
+            else:
+                print("Invalid password. Transaction canceled.")
+                return False
+        else:
+            print("Invalid PIN. Transaction canceled.")
+            return False
+        
 
 """ all driver code"""
    
@@ -63,15 +106,36 @@ print(f"{user.name} {user.pin} {user.password}")
 # print(f"{bankBob.name} {bankBob.pin} {bankBob.password} {bankBob.balance}")
 
 """ Driver Code for Task 4"""
-#Instantiate an object of the BankUser class
+# #Instantiate an object of the BankUser class
+# bankBob = BankUser("Bob", 1234, "password") 
+# # Call the show_balance method to print the balance
+# bankBob.show_balance()
+# # deposit 1000 to the balance
+# bankBob.deposit(1000)
+# # Call the show_balance method to print the balance
+# bankBob.show_balance()
+# # withdraw 500 from the balance
+# bankBob.withdraw(500)
+# # Call the show_balance method to print the balance
+# bankBob.show_balance()
+
+""" Driver Code for Task 5"""
+# Instantiate two objects of the BankUser class
 bankBob = BankUser("Bob", 1234, "password") 
-# Call the show_balance method to print the balance
+bankAlice = BankUser("Alice", 4321, "newpassword")
+# Deposit $5000 into Alice's account
+bankAlice.deposit(5000)
+# Show the balance of both accounts
+bankAlice.show_balance()
 bankBob.show_balance()
-# deposit 1000 to the balance
-bankBob.deposit(1000)
-# Call the show_balance method to print the balance
+# Have Alice transfer $500 to the Bob
+is_tranfer_successful = bankAlice.transfer_money(500, bankBob)
+# Show the balance of both accounts after the transfer
+bankAlice.show_balance()  
 bankBob.show_balance()
-# withdraw 500 from the balance
-bankBob.withdraw(500)
-# Call the show_balance method to print the balance
-bankBob.show_balance()
+# if the transfer was successful, have Bob request $250 from Alice
+if is_tranfer_successful:
+    is_request_successful = bankBob.request_money(250, bankAlice)
+    # Show the balance of both accounts after the request
+    bankAlice.show_balance()  
+    bankBob.show_balance()
