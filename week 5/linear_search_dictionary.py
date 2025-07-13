04ab70eab8a0699a0524efc6064d20a582e52dcc
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-13 17:43:13 -0400

    Code Challenge: Linear Search Dictionary

--- week 5/linear_search_dictionary.py
+++ week 5/linear_search_dictionary.py
@@ -5,4 +5,20 @@
 """
 
 def linear_search_dictionary(dictionary, target):
-    
+    """
+    Perform a linear search in a dictionary to find the target value.
+    """
+    for key in dictionary:
+        if dictionary[key] == target:
+            print(f"Found at key {key}")
+            return key
+    print("Target is not in the dictionary")
+    return None
+
+
+""" driver code """
+
+my_dictionary = {"red": 5, "blue": 3, "yellow": 12, "green": 7}
+linear_search_dictionary(my_dictionary, 5)
+linear_search_dictionary(my_dictionary, 3)
+linear_search_dictionary(my_dictionary, 8)
