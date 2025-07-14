65a1cc063ad678eddd60646d3e5cdec796a045fd
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-14 00:59:13 -0400

    FizzBuzz

---
+++ week 5/fizzbuzz.py
@@ -0,0 +1,17 @@
+""" """
+
+def fizzbuzz(num):
+    """"Print 'Fizz' if num is divisible by 3, print 'Buzz' if num is divisible by 5, and print 'FizzBuzz' if num is divisible by both."""
+
+    for i in range(1, num + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+""" driver code """
+fizzbuzz(50)
