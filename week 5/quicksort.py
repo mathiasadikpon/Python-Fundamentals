fd954109d3d7d3c15077b6b2c81e120c88368451
Mathias Adikpon <mathiasadikpon@gmail.com>
2025-07-13 23:05:33 -0400

    sort_part

---
+++ week 5/quicksort.py
@@ -0,0 +1,22 @@
+"""
+quick sort algorithm
+by Mathias Adikpon
+last updated: 07/13/2025
+"""
+
+def sort_part(the_list, low_idx, pivot_idx):
+    pivot_val = the_list[pivot_idx]
+
+    while pivot_idx != low_idx:
+        low_val = the_list[low_idx]
+
+        print(the_list, low_val, pivot_val)
+        if low_val <= pivot_val:
+            low_idx += 1
+        else:
+            the_list[low_idx] = the_list[pivot_idx-1]
+            the_list[pivot_idx] = low_val
+            the_list[pivot_idx-1] = pivot_val
+            pivot_idx -= 1
+
+    return pivot_idx
